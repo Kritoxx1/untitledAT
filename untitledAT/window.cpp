9bb46d0730e6138159f581e2a88f44d6180a065e
Alex <xil743@gmail.com>
2026-03-28 22:32:03 +0100

    Window Klasse

---
+++ untitledAT/window.cpp
@@ -0,0 +1,22 @@
+#include "window.hpp"
+Window::Window(const char* name, int width, int height) {
+	m_name = name;
+	m_height = height;
+	m_width = width;
+
+
+	/* Initialize the library */
+	glfwInit();
+		
+	/* Create a windowed mode window and its OpenGL context */
+	m_window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
+	if (!m_window)
+	{
+		glfwTerminate();
+		
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(m_window);
+
+};
---
+++ untitledAT/window.hpp
@@ -0,0 +1,14 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+#include <GLFW/glfw3.h>
+class Window {
+private:
+	const char* m_name;
+	int m_width;
+	int m_height;
+	
+public:
+	GLFWwindow* m_window;
+	Window(const char* name, int width, int height);
+};
+#endif
